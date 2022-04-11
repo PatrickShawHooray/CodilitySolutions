8eb33029a55e35af3a619930ac462e59fc3697e1
Patrick Shaw <Patrick.Shaw@outlook.com>
2022-04-10 23:16:46 -0700

    added PermCheck

---
+++ CodilitySolutions/PermCheck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodilitySolutions
+{
+    static public class PermCheck
+    {
+        public static int solution(int[] A)
+        {
+            if (A.Length == 0)
+                return 1;
+
+            if (A.Length == 1 && A[0] != 1)
+                return 0;
+
+            // use hashset to check for missing element via the length of the array
+            HashSet<int> toFind = new HashSet<int>();
+            for(int i = 0; i < A.Length; i++)
+            {
+                toFind.Add(A[i]);
+            }
+
+            // check if number in range is missing from toFind
+            for (int i = 1; i <= A.Length; i++)
+            {
+                if (!toFind.Contains(i))
+                    return 0;
+            }
+
+            return 1;
+        }
+    }
+}
