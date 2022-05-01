fd1d89a7c240b492f6b03903a5b05bacd8213c82
Patrick Shaw <Patrick.Shaw@outlook.com>
2022-05-01 16:01:24 -0700

    added CountFactors

---
+++ CodilitySolutions/CountFactors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodilitySolutions
+{
+    public static class CountFactors
+    {
+        public static int solution(int N)
+        {
+            if (N == 1)
+                return 1;
+
+            int ret = 0;
+            int i = 1;
+
+            // search up to sqrt; +2 for each find of modulus 0
+            int sqrt = (int)Math.Sqrt(N); 
+            while(i <= sqrt)
+            {
+                if (N % i == 0)
+                    ret += 2;
+                i++;
+            }
+
+            // check for perfect square (one to many if that's true)
+            if (sqrt * sqrt == N)
+                ret--;
+
+            return ret;
+        }
+    }
+}
