346c832a2880796d74ff6afb5f976b4a513774aa
Patrick Shaw <Patrick.Shaw@outlook.com>
2022-05-02 17:03:31 -0700

    Added MinPerimeterRectangle

---
+++ CodilitySolutions/MinPerimeterRectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodilitySolutions
+{
+    static public class MinPerimeterRectangle
+    {
+        // least difference between height and width is what we are after
+        // 2 factors closest to sqrt will give us that
+
+        static public int solution(int N)
+        {
+            int sqrt = (int)Math.Sqrt(N);
+
+            // check for square
+            if (sqrt * sqrt == N)
+                return sqrt * 4;
+
+            int i = sqrt;
+            int h = 0;
+            int w = 0;
+
+            // find largest factor starting at square root
+            while (i >= 1 && h == 0)
+            {
+                if (N % i == 0)
+                {
+                    h = i;
+                    w = N / h;
+                }
+                i--;
+            }
+
+            // for values where the only factors are 1 and thhe number
+            if(h == 0 || w == 0)
+            {
+                h = 1;
+                w = N;
+            }
+
+            return (h * 2) + (w * 2);
+        }
+    }
+}
--- RunColditilyExamples/Program.cs
+++ RunColditilyExamples/Program.cs
@@ -13,7 +13,7 @@ namespace RunColditilyExamples
             int[] tenThousand = Enumerable.Range(1, 10000).ToArray();
             int[] thousandStartNeg500 = Enumerable.Range(-500, 1000).ToArray();
 
-            Console.WriteLine(CodilitySolutions.CountFactors.solution(24));
+            Console.WriteLine(CodilitySolutions.MinPerimeterRectangle.solution(48));
         }
     }
 }
