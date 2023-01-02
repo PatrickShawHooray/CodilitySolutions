78b5a08c42ded7a6e555f478fc383c591fc4380c
Patrick Shaw <Patrick.Shaw@outlook.com>
2023-01-02 12:10:31 -0800

    test file commit

---
+++ CodilitySolutions/AtestFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodilitySolutions
+{
+    internal class AtestFile
+    {
+        // this is a test file
+    }
+}
